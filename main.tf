9b6e305b7128870f55ecd832f14b5041f01aedcb
ozy-ahmad <ahmadfakhrozy@gmail.com>
2021-11-28 03:51:10 +0000

    ports

--- main.tf
+++ main.tf
@@ -19,6 +19,10 @@ resource "docker_container" "grafana_container" {
   name  = "grafana_container-${count.index}"
   image = docker_image.grafana_image.latest
   count = 2
+  ports {
+    internal = var.int_port
+    external = var.ext_port[count.index]
+  }
 }
 
 # output "public_ip" {
@@ -26,5 +30,5 @@ resource "docker_container" "grafana_container" {
 # }
 
 output "public_ip" {
-    value =  [for x in docker_container.grafana_container : "${x.name} - ${x.ip_address}"]
+  value = [for x in docker_container.grafana_container : "${x.name} - ${x.ip_address}:${x.ports[0].external}"]
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,10 @@
 variable "int_port" {
-    default = 3000
-    validation {
-        condition = var.int_port == 3000
-        error_message = "The Grafana port must be set to 3000."
-    }
+  # default = 3000
+  validation {
+    condition     = var.int_port == 3000
+    error_message = "The Grafana port must be set to 3000."
+  }
+}
+variable "ext_port" {
+
 }
